1763bfed245067946a9f8cf10b703a84c69a6266
arect <40746088+arect@users.noreply.github.com>
2020-10-26 10:42:11 +0800

    传说中的模块化
    我什么都不知道随便说的。

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.write("<script type='text/javascript' src='./js/about.js'></script>");
+document.write("<script type='text/javascript' src='./js/cat.js'></script>");
+document.write("<script type='text/javascript' src='./js/cd.js'></script>");
+document.write("<script type='text/javascript' src='./js/clear.js'></script>");
+document.write("<script type='text/javascript' src='./js/help.js'></script>");
+document.write("<script type='text/javascript' src='./js/html.js'></script>");
+document.write("<script type='text/javascript' src='./js/ls.js'></script>");
+document.write("<script type='text/javascript' src='./js/shutdown.js'></script>");
