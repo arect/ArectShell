3539ea2a762fce389ad664e77f269abbe0e9a7e8
inspi <inspiration2030@outlook.com>
2020-10-30 17:36:07 +0800

    bug fixed

--- js/main.js
+++ js/main.js
@@ -7,3 +7,4 @@ document.write("<script type='text/javascript' src='./js/html.js'></script>");
 document.write("<script type='text/javascript' src='./js/ls.js'></script>");
 document.write("<script type='text/javascript' src='./js/screenfetch.js'></script>");
 document.write("<script type='text/javascript' src='./js/shutdown.js'></script>");
+document.write("<script type='text/javascript' src='./js/template.js'></script>");
