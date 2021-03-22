f6f33f41f536a58f2a19e19fd84e0353a9ad9ca8
inspi <inspiration2030@outlook.com>
2021-03-22 23:30:39 +0800

    完善sudo，现在几乎可以正常使用了

--- js/sudo.js
+++ js/sudo.js
@@ -2,17 +2,25 @@ let c_sudo = new Vue({
     data: {
         name: "sudo",
         usage: "[]",
-        description: "sudo"
+        description: "sudo",
+        command: []
     },
     methods: {
         main: function (arr) {
+            if (mode.sign === "root") {
+                this.command.splice(0, 1)
+                inputCommand.select(this.command)
+                this.command = []
+                mode.normal()
+            }
             for (let i of shell.users) {
                 if (i.name === prompt.username) {
-                    console.log(arr)
-                    console.log(i.password)
-                    if (arr[0] === i.password) {
-                        mode.isActive = false
-                        return [{isHtml: false, result: "Success"}]
+                    if (arr === i.password) {
+                        mode.sign = "root"
+                        this.command.splice(0, 1)
+                        inputCommand.select(this.command)
+                        this.command = []
+                        mode.normal()
                     }
                 }
             }
